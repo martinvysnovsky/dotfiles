b2b1581b73f322a92d95a1aac22082819bf9b0ce
Martin Vyšňovský <martinvysnovsky@gmail.com>
2024-01-10 18:15:18 +0100

    Use node v20 by default

--- run_once_checkout_latest_nvm.sh
+++ run_once_checkout_latest_nvm.sh
@@ -7,6 +7,7 @@ git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --
 . ~/.nvm/nvm.sh
 
 # install latest node
-nvm install node
+nvm install 20
 nvm install-latest-npm
+nvm alias default 20
 
