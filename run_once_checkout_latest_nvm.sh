3ac0178f79e91644620f03c0736946067ca6b0af
Martin Vyšňovský <martinvysnovsky@gmail.com>
2024-02-13 07:49:45 +0100

    Install global neovim package for node

--- run_once_checkout_latest_nvm.sh
+++ run_once_checkout_latest_nvm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/.nvm
-git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
 
 # activate nvm
 . ~/.nvm/nvm.sh
@@ -11,3 +11,5 @@ nvm install 20
 nvm install-latest-npm
 nvm alias default 20
 
+# install global packages
+npm install -g neovim
