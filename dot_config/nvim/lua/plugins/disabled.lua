f66bd472934529126c7a5a35ebe34d02a8f9b67f
Martin Vyšňovský <martinvysnovsky@gmail.com>
2024-07-23 09:11:16 +0200

    Disable copilot

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,6 @@ return {
 	{ "folke/flash.nvim", enabled = false },
 	{ "echasnovski/mini.pairs", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
+	{ "zbirenbaum/copilot.lua", enabled = false },
+	{ "zbirenbaum/copilot-cmp", enabled = false },
 }
