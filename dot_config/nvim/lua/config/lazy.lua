585e10790696ea2954b44f1193f8f9b84172e38e
Martin Vyšňovský <martinvysnovsky@gmail.com>
2024-08-08 09:11:15 +0200

    Enable refactoring extra in nvim

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lsp.none-ls" },
 		{ import = "lazyvim.plugins.extras.test.core" },
+		{ import = "lazyvim.plugins.extras.editor.refactoring" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
