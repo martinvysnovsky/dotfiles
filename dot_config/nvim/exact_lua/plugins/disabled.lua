a86cf7d248711f674ad44d381eed3a797d3a8451
Martin Vysnovsky <martinvysnovsky@gmail.com>
2025-01-05 21:48:04 +0100

    Remove copilot from disabled packages

--- dot_config/nvim/exact_lua/plugins/disabled.lua
+++ dot_config/nvim/exact_lua/plugins/disabled.lua
@@ -2,6 +2,4 @@ return {
 	{ "folke/flash.nvim", enabled = false },
 	{ "echasnovski/mini.pairs", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
-	{ "zbirenbaum/copilot.lua", enabled = false },
-	{ "zbirenbaum/copilot-cmp", enabled = false },
 }
