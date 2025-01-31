d7844cd42083d20cbc80e4e8721864eecbfd6692
Martin Vysnovsky <martinvysnovsky@gmail.com>
2025-01-31 10:32:29 +0100

    Use less powerfull model for completions

--- dot_config/nvim/exact_lua/exact_plugins/minuet-ai.lua
+++ dot_config/nvim/exact_lua/exact_plugins/minuet-ai.lua
@@ -3,20 +3,21 @@ return {
 	config = function()
 		require("minuet").setup({
 			notify = "warn",
-			context_window = 16000,
-			n_completions = 1,
 			provider = "openai_fim_compatible",
+			n_completions = 1,
+			context_window = 512,
 			provider_options = {
 				openai_fim_compatible = {
 					api_key = "TERM",
 					name = "Ollama",
 					end_point = "http://localhost:11434/v1/completions",
-					model = "qwen2.5-coder:14b",
+					model = "qwen2.5-coder:3b",
 					stream = true,
 					optional = {
 						max_tokens = 256,
 						top_p = 0.9,
-						stop = { "\n" },
+						stop = { "\n\n" },
+						-- stop = { "\n" },
 					},
 				},
 			},
