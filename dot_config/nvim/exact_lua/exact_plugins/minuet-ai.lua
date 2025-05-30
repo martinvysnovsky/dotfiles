638ad9277dc7dad26227522b0b6d0a99b73af9be
Martin Vysnovsky <martinvysnovsky@gmail.com>
2025-05-30 10:44:21 +0200

    Use deepseek

--- dot_config/nvim/exact_lua/exact_plugins/minuet-ai.lua
+++ dot_config/nvim/exact_lua/exact_plugins/minuet-ai.lua
@@ -4,13 +4,13 @@ return {
 		require("minuet").setup({
 			provider = "openai_fim_compatible",
 			n_completions = 1,
-			context_window = 512,
+			context_window = 1024,
 			provider_options = {
 				openai_fim_compatible = {
 					api_key = "TERM",
 					name = "Ollama",
 					end_point = "http://localhost:11434/v1/completions",
-					model = "qwen2.5-coder:7b-instruct-q8_0",
+					model = "deepseek-coder-v2:latest",
 					optional = {
 						max_tokens = 56,
 						top_p = 0.9,
