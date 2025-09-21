2f4a1c33c8ca1531fc9c82f3d01a19c64cbe3d13
Martin Vysnovsky <martinvysnovsky@gmail.com>
2025-09-21 08:58:33 +0200

    fix(nvim): correct mini.pairs plugin namespace in disabled plugins

--- dot_config/nvim/exact_lua/exact_plugins/disabled.lua
+++ dot_config/nvim/exact_lua/exact_plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
 	{ "folke/flash.nvim", enabled = false },
-	{ "echasnovski/mini.pairs", enabled = false },
+	{ "nvim-mini/mini.pairs", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
 }
