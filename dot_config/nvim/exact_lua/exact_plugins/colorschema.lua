8441347cb46a55cc1941fe2c3665293bddab9713
Martin Vysnovsky <martinvysnovsky@gmail.com>
2026-04-22 08:23:58 +0200

    chore(nvim): configure catppuccin flavor and neotree integration

--- dot_config/nvim/exact_lua/exact_plugins/colorschema.lua
+++ dot_config/nvim/exact_lua/exact_plugins/colorschema.lua
@@ -1,5 +1,15 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+			integrations = {
+				neotree = true,
+			},
+		},
+	},
 
 	{
 		"LazyVim/LazyVim",
