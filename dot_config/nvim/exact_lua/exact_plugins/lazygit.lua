ef2fa0e8938c215347fddfccb02b4fa54aa2b3c1
Martin Vysnovsky <martinvysnovsky@gmail.com>
2026-05-08 14:51:14 +0200

    refactor(nvim): remove redundant vertical navigation keybindings from lazygit
    Remove nav_j and nav_k keybindings as they conflict with terminal mode navigation and are redundant with existing horizontal navigation.

--- dot_config/nvim/exact_lua/exact_plugins/lazygit.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lazygit.lua
@@ -13,14 +13,6 @@ return {
 						if self:is_floating() then vim.fn.system("tmux select-pane -L")
 						else vim.cmd.wincmd("h") end
 					end, desc = "Navigate Left", mode = "t" },
-					nav_j = { "<C-j>", function(self)
-						if self:is_floating() then vim.fn.system("tmux select-pane -D")
-						else vim.cmd.wincmd("j") end
-					end, desc = "Navigate Down", mode = "t" },
-					nav_k = { "<C-k>", function(self)
-						if self:is_floating() then vim.fn.system("tmux select-pane -U")
-						else vim.cmd.wincmd("k") end
-					end, desc = "Navigate Up", mode = "t" },
 					nav_l = { "<C-l>", function(self)
 						if self:is_floating() then vim.fn.system("tmux select-pane -R")
 						else vim.cmd.wincmd("l") end
