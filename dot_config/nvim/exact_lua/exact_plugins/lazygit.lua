ba95f59ec9015b8956c9239a7183dd554fbb13db
Martin Vysnovsky <martinvysnovsky@gmail.com>
2026-05-08 08:26:30 +0200

    feat(nvim): add lazygit terminal navigation keybindings
    Add Ctrl+hjkl navigation keybindings for lazygit terminal mode. When floating, delegates to tmux pane navigation; otherwise uses vim window commands.

--- dot_config/nvim/exact_lua/exact_plugins/lazygit.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lazygit.lua
@@ -6,5 +6,27 @@ return {
 				inactiveBorderColor = { fg = "Comment" },
 			},
 		},
+		terminal = {
+			win = {
+				keys = {
+					nav_h = { "<C-h>", function(self)
+						if self:is_floating() then vim.fn.system("tmux select-pane -L")
+						else vim.cmd.wincmd("h") end
+					end, desc = "Navigate Left", mode = "t" },
+					nav_j = { "<C-j>", function(self)
+						if self:is_floating() then vim.fn.system("tmux select-pane -D")
+						else vim.cmd.wincmd("j") end
+					end, desc = "Navigate Down", mode = "t" },
+					nav_k = { "<C-k>", function(self)
+						if self:is_floating() then vim.fn.system("tmux select-pane -U")
+						else vim.cmd.wincmd("k") end
+					end, desc = "Navigate Up", mode = "t" },
+					nav_l = { "<C-l>", function(self)
+						if self:is_floating() then vim.fn.system("tmux select-pane -R")
+						else vim.cmd.wincmd("l") end
+					end, desc = "Navigate Right", mode = "t" },
+				},
+			},
+		},
 	},
 }
